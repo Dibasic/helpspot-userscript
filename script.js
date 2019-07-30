d0e486e758c735d28861f4872df2e7513bdb580e
Dibasic <Dibasic@users.noreply.github.com>
2019-07-30 15:03:29 -0500

    refactor startTimer and styleFunctions, linter pass, version advance

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 // ==UserScript==
 // @name         HelpSpot styling
 // @namespace    helpspot
-// @version      0.96
+// @version      0.97
 // @description  style helpspot interface
 // @author       Ethan Jorgensen
 // @include      /^https?://helpspot\.courseleaf\.com/admin\.php\?pg=(?:workspace|request(?:&fb=\d+)?)&(?:show|reqid)=(\w+)/
 // @grant        none
 // ==/UserScript==
 
-/*  jshint esversion: 6
-  , laxcomma: true
-  , laxbreak: true
-  , -W069 */
+// jshint devel: true, esnext: true, laxcomma: true, laxbreak: true, -W069
 (function() {
     'use strict';
 
@@ -50,12 +47,15 @@
     }
 
     function startTimer() {
-        let loading = document.getElementById('hs_msg');
-        if (!loading.style.cssText.includes('display: none')) {
-            console.log('detecting loading message - running runStyleFunctions()');
+        // Just any condition that helps us tell if we've styled the page already or not.
+        let headers = document.querySelector('tr.tableheaders');
+        if (!headers.getAttribute('token')) {
+            console.log('detected refresh - running runStyleFunctions()');
             runStyleFunctions();
+            // Set that condition again
+            headers.setAttribute('token', true);
         }
-        setTimeout(startTimer, 1000);
+        setTimeout(startTimer, 200);
     }
 
     function setColors() {
@@ -104,10 +104,10 @@
 
         colors.error       = colors.split1_d;   // #b93d3c
         colors.warning     = colors.conyellow;  // #dddd49
+        colors.resolved    = colors.triad2_d;   // #7ab93c
         colors.feature     = colors.base;       // #70a0d1
         colors.waiting     = colors.split1_l;   // #dd9797
         colors.question    = colors.triad1;     // #d170a0
-        colors.resolved    = colors.triad2_d;   // #7ab93c
 
         colors.pub         = colors.triad2_d;   // #7ab93c
         colors.prv         = colors.split1_d;   // #b93d3c
@@ -212,7 +212,7 @@
 
     // global stylings to run in both workspaces and requests
     function global() {
-        styleFunctions['noradius'] = function() {
+        styleFunctions.noradius = function() {
             function styleNoBorder(e) {
                 e.style['border-radius'] = '0';
                 e.style['-webkit-border-radius'] = '0';
@@ -221,7 +221,7 @@
 
             document.querySelectorAll('.btn').forEach(styleNoBorder);
         };
-        styleFunctions['nogradient'] = function() {
+        styleFunctions.nogradient = function() {
             let count = 0;
             function styleNoGradient(e, flatcolor) {
                 e.style['background'] = flatcolor;
@@ -247,7 +247,7 @@
 
             return count;
         };
-        styleFunctions['noborder'] = function() {
+        styleFunctions.noborder = function() {
             let count = 0;
             function styleNoBorder(e) {
                 e.style['border'] = 'none';
@@ -258,7 +258,7 @@
 
             return count;
         };
-        styleFunctions['noshadow'] = function() {
+        styleFunctions.noshadow = function() {
 
             let count = 0;
             function styleNoShadow(e, bold=false) {
@@ -293,7 +293,7 @@
             return count;
         };
         // todo move out of stylefunctions, only needs to run once
-        styleFunctions['tabevents'] = function() {
+        styleFunctions.tabevents = function() {
 
             function tabActivate(e) {
                 let active = e.className === 'active';
@@ -351,7 +351,8 @@
 
             return result.length;
         };
-        styleFunctions['category'] = function() {
+
+        styleFunctions.category = function() {
             const pattern = /^(?:([A-Z]{2,})(?=$| (\d)| Client (Q)| (SOW)| (Mile))|(Impl|Other|Sales|daily\.sh|User Com)).*/
             , sub = '$1$6 $2$3$4$5';
 
@@ -395,7 +396,7 @@
 
             return result.length;
         };
-        styleFunctions['cid'] = function() {
+        styleFunctions.cid = function() {
             function styleCidCell(e) {
                 e.style['font-weight'] = 'bold';
             }
@@ -406,11 +407,11 @@
             if (header) {
                 header.innerText = 'Client';
             }
-            result.forEach(c => {c.style['font-weight'] = 'bold';});
+            result.forEach(styleCidCell);
 
             return result.length;
         };
-        styleFunctions['age'] = function() {
+        styleFunctions.age = function() {
             const age = /^(\d{1,2}) ([mhdw])(?:in|our|ay|eek|onth)s?(?:, (\d{1,2}) ([mhdw])(?:in|our|ay|eek|onth)s?)?$/;
             function formatAge(a) {
                 let match = a.match(age);
@@ -434,7 +435,7 @@
 
             return result.length;
         };
-        styleFunctions['numUpdates'] = function() {
+        styleFunctions.numUpdates = function() {
             let result = getColumnById('1_table_header_ctPublicUpdates').cells;
             result.forEach(c => {
                 c.innerText = c.innerText === '1' ? '' : c.innerText;
@@ -444,13 +445,13 @@
 
             return result.length;
         };
-        styleFunctions['status'] = function() {
+        styleFunctions.status = function() {
             const pattern = /^(?:Pending (Client Feedback|Internal Info)|Support Rep (Working)|Problem (Solved)|Question (Answered)|(App)ointment( Scheduled| Complete)|Customer (Found Solution|Unreachable)|Passed to (Implementation)|(Sales) Request)$/
             , sub = '$1$2$3$4$5$6$7$8$9';
 
             function styleStatusCell(e) {
                 if (!e.title) {
-                    let status = getCustomStatus(getId(e))
+                    let status = getCustomStatus(getId(e));
                     if (status) {
                         e.title = status;
                         styleElement(e, 'text-decoration: underline; text-decoration-style: dotted');
@@ -525,7 +526,7 @@
 
             return result.length;
         };
-        styleFunctions['email'] = function() {
+        styleFunctions.email = function() {
             let column = getColumnById('1_table_header_sEmail')
             , header = column.header
             , result = column.cells;
@@ -543,7 +544,7 @@
 
             return result.length;
         };
-        styleFunctions['request'] = function() {
+        styleFunctions.request = function() {
             function styleRequestCell(e) {
                 setHoverText(e);
                 setFontSize(e);
@@ -562,7 +563,7 @@
 
             return result.length;
         };
-        styleFunctions['inboxlabel'] = function() {
+        styleFunctions.inboxlabel = function() {
             function styleInboxLabelCell(e) {
                 if (e.innerText.startsWith('Courseleaf ')) {
                     e.innerText = e.innerText.substring(11);
@@ -581,17 +582,20 @@
 
         console.log('> Workspace view detected. Applying workspace styling.');
 
+        startTimer();
+
     }
 
     function request() {
-        eventFunctions['reqbuttons'] = function() {
+
+        eventFunctions.reqbuttons = function() {
             function addRequestButtonEvent(e) {
-                e.addEventListener('click', styleFunctions['reqbuttons']);
+                e.addEventListener('click', styleFunctions.reqbuttons);
             }
             document.querySelectorAll('.request-sub-note-box > button').forEach(addRequestButtonEvent);
         };
 
-        eventFunctions['tabreset'] = function() {
+        eventFunctions.tabreset = function() {
             document.querySelector('a[href^="#livelookup"]').addEventListener('click', function() {
                 setTimeout(function() {
                     document.querySelector('#customer_ajax_ll_inner > div.box_footer > button').addEventListener('click', function() {
@@ -603,7 +607,7 @@
             })
         }
 
-        eventFunctions['inboxselect'] = function() {
+        eventFunctions.newrequest = function() {
             let inbox = document.getElementById('Custom22');
             if (!inbox.value) {
                 inbox.value = inbox.options[1].value;
@@ -632,7 +636,7 @@
             }
         };
 
-        styleFunctions['reqbuttons'] = function() {
+        styleFunctions.reqbuttons = function() {
             styleSelectorAll('.request-sub-note-box > button', `min-width: 75px; background: ${colors.gray_l} !important; text-shadow: none !important; font-weight: normal !important; background-image: none !important`);
             let color;
             if (1 == styleSelector('#button-public.btn-request-public',     `background: ${colors.pub} !important; font-weight: bold !important`)) {
@@ -685,14 +689,13 @@
     }
 
     function runStyleFunctions() {
-        Object.keys(styleFunctions).forEach(f => console.log('> > ' + f + ' updated ' + styleFunctions[f].call()) + ' elements');
+        Object.keys(styleFunctions).forEach(fn => console.log('> > ' + fn + ' updated ' + styleFunctions[fn].call() + ' elements'));
     }
 
     function runEventFunctions() {
-        Object.keys(eventFunctions).forEach(f => console.log('> > ' + f + ' created event for ' + eventFunctions[f].call()) + ' elements');
+        Object.keys(eventFunctions).forEach(fn => console.log('> > ' + fn + ' created event for ' + eventFunctions[fn].call() + ' elements'));
     }
 
     main();
-    startTimer();
 
 })();
