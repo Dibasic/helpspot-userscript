7df3c0a91e7fc4a54df73cac9cbc78d37d371746
Dibasic <Dibasic@users.noreply.github.com>
2019-10-16 15:23:16 -0500

    Fix $.css() JSON syntax

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         HelpSpot styling
 // @namespace    hssu
-// @version      1.04.04_dev
+// @version      1.04.05_dev
 // @description  style helpspot interface
 // @author       Ethan Jorgensen
 // @include      /^https?:\/\/helpspot\.courseleaf\.com\/admin\.php\?pg=(?:workspace(?:&filter=created=[^&]+)?(?:&show=([^&]+))?(?:&fb=[^&]+)?|request(?:\.static)?(?:&fb=([^&]+))?(?:&reqid=([^&]+)))?/
@@ -922,8 +922,8 @@
 
             });
             $('hssu-wysiwyg-lbl').css({
-                'font-size', '8px';
-                'text-transform', 'uppercase'
+                'font-size': '8px';
+                'text-transform': 'uppercase'
             });
 
             // styles for outer div
